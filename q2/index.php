6054d5420c5e943f3b43585a8b0d32b1cbd785cd
evechen1226 <eveliving2021@gmail.com>
2023-12-01 09:35:53 +0800

    新增題組二-index.php

---
+++ q2/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+</head>
+<body>
+    <header class="p-5">
+<h1 class="text-center">問卷調查</h1>
+</header>
+<main class="container">
+    <fieldset>
+        <legend>目前位置：首頁 > 問內卷調查</legend>
+        <!-- table>(tr>th*5)+(tr>td*5) 要自已打才有效果-->
+        
+        <table class="table table-striped">
+            <tr>
+                <th>編號</th>
+                <th>問卷題</th>
+                <th>投票總數</th>
+                <th>結果</th>
+                <th>狀態</th>
+
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+
+        </table>
+    </fieldset>
+</main>
+
+<script src="../js/jquery-3.4.1.min.js"></script>
+<script src="../js/js.js"></script>
+<script src="../js/bootstrap.js"></script>
+</body>
+</html>
