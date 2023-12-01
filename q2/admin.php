fc2726543ea0febd46d4a3f833f759fad9f4e2c8
evechen1226 <eveliving2021@gmail.com>
2023-12-01 09:39:09 +0800

    新增問卷管-admin.php

---
+++ q2/admin.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>問卷管理後台</title>
+    <link rel="stylesheet" href="../css/bootstrap.css">
+</head>
+<body>
+    <header class="container">
+        <h1 class="text-center">問卷管理</h1>
+    </header>
+    <main class="container"></main>
+
+
+    <script src="../js/jquery-3.4.1.min.js"></script>
+    <script src="../js/js.js"></script>
+    <script src="../js/bootstrap.js"></script>
+</body>
+</html>
