af264d00c3c1ce59a3d57010e86cc652aa4af161
evechen1226 <eveliving2021@gmail.com>
2023-12-01 11:25:30 +0800

    feat:完成問卷新增功能

---
+++ q2/add_que.php
@@ -0,0 +1,27 @@
+<?php
+include_once "./db.php";
+echo dd($_POST);
+
+$date = [];
+$date['text'] = $_POST['subject'];
+$date['subject_id'] = 0; //預設 0 為 主題, 因為 id 沒有從 0 開始
+$date['conut'] = 0;
+$date['sh'] = 1;
+
+$Que->save($date);
+
+foreach ($_POST['opt'] as $opt) {
+    
+    $date = [];
+    $date['text'] = $_POST['subject'];
+
+    //如果要利用find()找id，要確認 subuject 沒有重複
+    $subject_id=$Que->find(['text'=>$_POST['subject']])['id'];
+    
+    $date['subject_id'] = 0; //預設 0 為 主題
+    $date['conut'] = 0;
+    $date['sh'] = 1;
+
+}
+
+
